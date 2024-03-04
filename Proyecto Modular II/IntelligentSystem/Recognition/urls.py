16fc02eb356e64b5561cf2f831f46c5792819a40
Guzman <guzmanjrpro@gmail.com>
2024-03-04 00:18:23 -0600

    A new path has been added for linking the corresponding methods

--- Proyecto Modular II/IntelligentSystem/Recognition/urls.py
+++ Proyecto Modular II/IntelligentSystem/Recognition/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.product_scan, name='product_scan'),
     path('video/', views.video, name='video'),
-    path('home/', views.stop_video, name='stop'),
+    path('stop_video/', views.stop_video, name='stop_video'),
+    path('restart_video/', views.restart_video, name='restart_video'),
 ]
