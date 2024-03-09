3647839b5c83a126347b81a281072b0fcb68d6bc
Guzman <guzmanjrpro@gmail.com>
2024-03-08 22:46:44 -0600

    Added modals for login and register and also added models.py

--- Proyecto Modular II/IntelligentSystem/API/models.py
+++ Proyecto Modular II/IntelligentSystem/API/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
+
+class administrador(models.Model):
+    Name = models.CharField(_("Name"), max_length=50)
+    Surname = models.CharField(_("Surname"), max_length=50)
+    Email = models.EmailField(_("Email"), max_length=254)
+    Password = models.CharField(_("Password"), max_length=50)
+    Cellular = models.CharField(_("Cellular"), max_length=10)
+    
+    class Meta:
+        db_table = 'administrador'
